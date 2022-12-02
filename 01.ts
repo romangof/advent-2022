0555c8925d5bc8a53660209342e30662100bce28
Roman <romangof@gmail.com>
2022-12-02 04:59:28 +0100

    Part 1 Added package.json to use FS Cleans comments

--- 01.ts
+++ 01.ts
@@ -1,3 +1,10 @@
+import * as fs from 'fs';
+
+const textFile = fs.readFileSync('./input', 'utf-8');
+
+// console.log(text);
+
+
 const sampleInput = `1000
 2000
 3000
@@ -13,29 +20,28 @@ const sampleInput = `1000
 
 10000`
 
-let separatedLines: string[] = sampleInput.split(/\r?\n|\r|\n/g);
+const separatedLines = (str: string) => str.split(/\r?\n|\r|\n/g);
 
-const maxOfSums = (str: string[]) => {
-    console.log('separated: ', separatedLines);
-    
-    const res = separatedLines.reduce((accumulator, current, index) => {
+const maxOfSums = (str: string[]) => {    
+    const res: number[] = str.reduce((accumulator, current, index) => {
         const localSum = Number(current) + accumulator[1]
         const maxTotal = (localSum > accumulator[0]) ? localSum : accumulator[0]
 
         if (!current) {
-            console.log(index, accumulator,  [maxTotal, 0],  `***${current}Empty***`);
+            // console.log(index, accumulator,  [maxTotal, 0],  `***${current}Empty***`);
 
             return [maxTotal, 0]
         }
 
-        console.log(index, accumulator, [maxTotal, localSum], `***${current}***`);
+        // console.log(index, accumulator, [maxTotal, localSum], `***${current}***`);
         
         return [maxTotal, localSum]
 
     }, [0, 0])
 
-    console.log('\nreduced: ', res, "res: ", res[0]);
+    return res[0]
 }
 
-console.log("------", maxOfSums(separatedLines));
+// console.log("------", maxOfSums(separatedLines)[0]);
+console.log("------", maxOfSums(separatedLines(textFile)));
  
