78e015febe2269ca01ccf2d9644626cf2a963639
Roman Fuentes <r.fuentes@porta.de>
2022-12-05 13:30:38 +0100

    Day 3 done!

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 
 ![Day](https://badgen.net/badge/01/%E2%98%86%E2%98%86/gray)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%86/yellow)](src/day03)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -83,9 +83,9 @@ Both parts: 2.0747850000000003ms
 
 ```
 Day 03
-Time part 1: 16.312ms
-Time part 2: -
-Both parts: 16.312032ms
+Time part 1: 0.348ms
+Time part 2: 0.235ms
+Both parts: 0.5825ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 3/50
-Total time: 18.387ms
+Total stars: 4/50
+Total time: 2.657ms
 ```
 
 <!--/RESULTS-->
--- src/day03/index.ts
+++ src/day03/index.ts
@@ -9,43 +9,55 @@ CrZsJsPPZsGzwwsLwLmpwMDw`
 
 const parseInput = (rawInput: string) => rawInput.split("\n")
 
+const alphabet = Array.from(Array(26))
+  .map((_, i) => i + 97)
+  .map((char) => String.fromCharCode(char))
+  .concat(
+    Array.from(Array(26))
+      .map((_, i) => i + 65)
+      .map((char) => String.fromCharCode(char)),
+  )
+
+const priority = (x: string) => alphabet.indexOf(x) + 1
+
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput).map((str) => [
     str.substring(0, str.length / 2),
     str.substring(str.length / 2),
   ])
 
-  const alphabet = Array.from(Array(26))
-    .map((_, i) => i + 97)
-    .map((char) => String.fromCharCode(char))
-    .concat(
-      Array.from(Array(26))
-        .map((_, i) => i + 65)
-        .map((char) => String.fromCharCode(char)),
-    )
-
-  const priority = (x: string) => alphabet.indexOf(x) + 1
-
-  const res = input
+  return input
     .map((sacks) =>
       Array.from(sacks[0]).reduce(
         (accum, current) => (sacks[1].includes(current) ? current : accum),
         "",
       ),
     )
-    .reduce((accum, current) => {
-      console.log(priority(current))
-
-      return accum + priority(current)
-    }, 0)
-
-  return res
+    .reduce((accum, current) => accum + priority(current), 0)
 }
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput)
+  const input = parseInput(rawInput).reduce(
+    (accum, current, index, array) =>
+      (index + 1) % 3 === 0
+        ? [...accum, [array[index - 2], array[index - 1], current]]
+        : accum,
+    [] as string[][],
+  )
 
-  return
+  return input
+    .map((trio) =>
+      priority(
+        Array.from(trio[0]).reduce(
+          (accum, current) =>
+            trio[1].includes(current) && trio[2].includes(current)
+              ? current
+              : accum,
+          "",
+        ),
+      ),
+    )
+    .reduce((a, b) => a + b)
 }
 
 run({
@@ -62,7 +74,7 @@ run({
     tests: [
       {
         input: sampleInput,
-        expected: "",
+        expected: 70,
       },
     ],
     solution: part2,
